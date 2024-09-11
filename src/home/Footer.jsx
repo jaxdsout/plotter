330fc67f0090ef0b1048b38df9c4fffc38c54bb7
jaxdsout <jaxon.southern@mac.com>
2024-09-11 12:54:25 -0500

    prepare for launch

--- src/home/Footer.jsx
+++ src/home/Footer.jsx
@@ -4,7 +4,7 @@ function Footer () {
     return (
         <nav className='navbar p-5 bg-body-tertiary mt-5'>
             <div className="container-fluid">
-                <p> link </p>
+                <p>  </p>
             </div>
         </nav>    
     )
