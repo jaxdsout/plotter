6007e9dbd0e7956e572ce2f96fd61cd75813b89b
jaxdsout <jaxon.southern@mac.com>
2025-01-22 10:41:18 -0600

    added deal edit feature

--- src/deals/DealDetail.jsx
+++ src/deals/DealDetail.jsx
@@ -3,8 +3,16 @@ import { connect } from "react-redux";
 import DeleteDeal from "./DeleteDeal.jsx";
 import { useEffect } from "react";
 import { load_deal, load_deals, update_deal_status } from "../store/actions/agent.js";
+import { set_deal_mode } from "../store/actions/ui.js";
+import EditDeal from "./EditDeal.jsx";
 
-function DealDetail ({ dealID, deal, handleCloseModal, update_deal_status, load_deal, load_deals, user }) {
+function DealDetail ({ dealID, deal, handleCloseModal, isDealMode, update_deal_status, set_deal_mode, load_deal, load_deals, user }) {
+    
+    const handleEditDeal = async () => {
+        if (user && deal) {
+            set_deal_mode()
+        }
+    }
 
     const formatDate = (dateStr) => {
         const dateObj = new Date(dateStr);
@@ -30,70 +38,81 @@ function DealDetail ({ dealID, deal, handleCloseModal, update_deal_status, load_
 
     return(
         <>
-            {deal ? (
-                <div className="flex flex-col mb-4">
-                    <div className="text-center">
-                        <p><b>Date Deal Created: </b>{formatDate(deal?.deal_date)}</p>
-                        <Divider />
-                    </div>
-                    <div className="flex flex-row justify-evenly">
-                        <div>
-                            <p><b>Client: </b>{deal?.client_name}</p>
-                            <p><b>Property: </b>{deal?.prop_name}</p>
-                            <p><b>Unit Number: </b>{deal?.unit_no}</p>
-                            <p><b>Lease Term: </b>{deal?.lease_term}</p>
-                            <p><b>Move Date: </b>{formatDate(deal?.move_date)}</p>
-                            <p><b>Rent: </b>${deal?.rent}</p>
-                            {deal?.rate ? (
-                                <p><b>Rate: </b>{deal?.rate}%</p>
-                            ) : ( 
-                                <p><b>Rate: </b>${deal?.flat_fee}</p>
-                            )}
-                            <p><b>Commission: </b>${deal?.commission}</p>
-                        </div>
-                        <div className="flex flex-col justify-between">
-                            <div>
-                                <p><b>Lease End Date: </b>{deal?.lease_end_date}</p>
-                                <p><b>Invoice Date: </b>{deal?.invoice_date}</p>
-                            </div>
-                            {deal?.status === 'not' ? (
-                                <Button onClick={() => changeStatus(deal?.id, 'pend')} color="yellow">SET INVOICED</Button>
-                            ) : deal.status === 'pend' ? (
-                                <Button onClick={() => changeStatus(deal?.id, 'paid')} color="green">SET PAID</Button>
-                            ) : deal.status === 'over' ? (
-                                <>
-                                    <Button disabled color="red">INVOICE OVERDUE</Button>
-                                    <Button onClick={() => changeStatus(deal?.id, 'paid')} color="green">SET PAID</Button>
-                                </>
-                            ) : (
-                                <></>
-                            )}
-                            {deal?.status === 'paid' ? (
-                                    <Button disabled color="green">INVOICE PAID</Button>
-                            ) : (
-                                <>
-                                </>
-                            )}
-                            <Button className="!bg-[#90B8F8] hover:!bg-[#5F85DB]">EDIT DEAL</Button>
-                            <DeleteDeal deal={deal} handleCloseModal={handleCloseModal}/>
-                        </div>  
-                    </div>
-                </div>
+            {isDealMode ? (
+                <>
+                    <EditDeal dealID={dealID}/>
+                </>
             ) : (
                 <>
-                    <Dimmer active>
-                        <Loader />
-                    </Dimmer>
-                </>
+                    {deal ? (
+                        <div className="flex flex-col mb-4">
+                            <div className="text-center">
+                                <p><b>Date Deal Created: </b>{formatDate(deal?.deal_date)}</p>
+                                <Divider />
+                            </div>
+                            <div className="flex flex-row justify-evenly">
+                                <div>
+                                    <p><b>Client: </b>{deal?.client_name}</p>
+                                    <p><b>Property: </b>{deal?.prop_name}</p>
+                                    <p><b>Unit Number: </b>{deal?.unit_no}</p>
+                                    <p><b>Lease Term: </b>{deal?.lease_term}</p>
+                                    <p><b>Move Date: </b>{formatDate(deal?.move_date)}</p>
+                                    <p><b>Rent: </b>${deal?.rent}</p>
+                                    {deal?.rate ? (
+                                        <p><b>Rate: </b>{deal?.rate}%</p>
+                                    ) : ( 
+                                        <p><b>Rate: </b>${deal?.flat_fee}</p>
+                                    )}
+                                    <p><b>Commission: </b>${deal?.commission}</p>
+                                </div>
+                                <div className="flex flex-col justify-start">
+                                    <div>
+                                        <p><b>Lease End Date: </b>{deal?.lease_end_date}</p>
+                                        <p><b>Invoice Date: </b>{deal?.invoice_date}</p>
+                                    </div>
+                                    <div className="flex flex-col mt-5">
+                                        {deal?.status === 'not' ? (
+                                            <Button onClick={() => changeStatus(deal?.id, 'pend')} color="yellow" className="!mb-4">SET INVOICED</Button>
+                                        ) : deal.status === 'pend' ? (
+                                            <Button onClick={() => changeStatus(deal?.id, 'paid')} color="green" className="!mb-4">SET PAID</Button>
+                                        ) : deal.status === 'over' ? (
+                                            <>
+                                                <Button disabled color="red" className="!mb-4">INVOICE OVERDUE</Button>
+                                                <Button onClick={() => changeStatus(deal?.id, 'paid')} color="green" className="!mb-4">SET PAID</Button>
+                                            </>
+                                        ) : (
+                                            <>
+                                            </>
+                                        )}
+                                        {deal?.status === 'paid' ? (
+                                            <Button disabled color="green" className="!mb-4">INVOICE PAID</Button>
+                                        ) : (
+                                            <>
+                                            </>
+                                        )}
+                                        <Button className="drop-shadow-sm text-nowrap !bg-[#90B8F8] hover:!bg-[#5F85DB] !mb-4" type="submit" onClick={handleEditDeal}>EDIT DEAL</Button>
+                                        <DeleteDeal deal={deal} handleCloseModal={handleCloseModal}/>
+                                    </div>    
+                                </div>  
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            <Dimmer active>
+                                <Loader />
+                            </Dimmer>
+                        </>
+                    )}
+                </> 
             )}
-            
         </>
     )
 }
 
 const mapStateToProps = state => ({
     user: state.auth.user,
-    deal: state.agent.deal
+    deal: state.agent.deal,
+    isDealMode: state.ui.isDealMode
 });
 
-export default connect(mapStateToProps, { update_deal_status, load_deal, load_deals })(DealDetail);
+export default connect(mapStateToProps, { update_deal_status, load_deal, load_deals, set_deal_mode })(DealDetail);
--- src/lists/ListDetail.jsx
+++ src/lists/ListDetail.jsx
@@ -1,18 +1,17 @@
-import { Button, Input, Divider, Form, Popup, Loader, Dimmer } from "semantic-ui-react";
+import { Button, Divider, Form, Popup, Loader, Dimmer } from "semantic-ui-react";
 import { reset_list_mode, set_list_mode } from "../store/actions/ui";
 import { connect } from "react-redux";
-import { set_search_client, update_list_options, new_option, set_list_edit, load_list} from "../store/actions/listmaker";
+import { new_option, load_list, update_list} from "../store/actions/listmaker";
 import PropertySearch from "../listmaker/PropertySearch";
 import OptionList from "../listmaker/OptionList";
 import MapBox from "../listmaker/MapBox";
 import ClearOptions from "../listmaker/ClearOptions";
 import DeleteList from "./DeleteList";
 import ReorderList from "../listmaker/ReorderList";
-import ShareURL from "../listmaker/ShareURL";
 import { useEffect } from "react";
 
-function ListDetail({ listID, list, options, property, client, user, set_list_mode, new_option, set_search_client, set_list_edit, handleCloseModal, 
-    update_list_options, reset_list_mode, load_list, isReorderMode, isListMode }) {
+function ListDetail({ listID, list, options, property, client, user, set_list_mode, new_option, handleCloseModal, 
+    update_list, reset_list_mode, load_list, isReorderMode, isListMode }) {
 
     const formatDate = (datetimeStr) => {
         const dateObj = new Date(datetimeStr);
@@ -28,27 +27,23 @@ function ListDetail({ listID, list, options, property, client, user, set_list_mo
 
     const handleEditList = async () => {
         if (user && list) {
-            const userID = user.id;
-            const name = user.full_name;
-            await set_search_client(userID, name)
-            set_list_edit(list)
             set_list_mode()
         }
     }
 
-
     const handleSaveList = async () => {
-        if (list && isListMode && client) {
-            await update_list_options(user.id, client.id, list, options)
+        if (isListMode) {
+            await update_list(user.id, list.client, list, options)
             await reset_list_mode()
-            load_list(listID)
+            load_list(list.id);
         }
     }
 
     const handlePropertyAdd = async (list, property) => {
         if (property && list) {
-            await new_option(property.id, list.id, client.id);
-            load_list(listID);
+            console.log("client id", list.client)
+            await new_option(property.id, list.id, list.client);
+            load_list(list.id);
         }
     };
 
@@ -69,110 +64,108 @@ function ListDetail({ listID, list, options, property, client, user, set_list_mo
         <>   
             {isListMode ? (
                 <>
-                  <div className="flex flex-col justify-evenly">
-                    <div className="">
-                        <div className="flex flex-row items-center justify-center">
-                            <PropertySearch />
-                            <Form onSubmit={() => handlePropertyAdd(list, property)} className="!ml-5">
-                                <Button color="blue" type="submit">ADD PROPERTY</Button>
-                            </Form>
+                    <div className="flex flex-col justify-evenly">
+                        <div className="">
+                            <div className="flex flex-row items-center justify-center">
+                                <PropertySearch />
+                                <Form onSubmit={() => handlePropertyAdd(list, property)} className="!ml-5">
+                                    <Button color="blue" type="submit">ADD PROPERTY</Button>
+                                </Form>
+                            </div>
+                            <Divider />
+                            <div>
+                                <OptionList />
+                            </div>
                         </div>
-                        <Divider />
                         <div>
-                            <OptionList />
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex items-center justify-center pt-7 pb-3">
-                            <MapBox />
-                        </div>
-                        <Divider />
-                        <div className="flex justify-center items-center">
-                            {isReorderMode ? (
-                                <>
-                                    <ReorderList />
-                                </>
-                            ) : (
-                                <>
-                                    <div>
-                                        <ClearOptions />
-                                    </div>
-                                    <div className="pr-2 pl-2">
+                            <div className="flex items-center justify-center pt-7 pb-3">
+                                <MapBox />
+                            </div>
+                            <Divider />
+                            <div className="flex justify-center items-center">
+                                {isReorderMode ? (
+                                    <>
                                         <ReorderList />
-                                    </div>
-                                    <Button className="drop-shadow-sm" color="green" type="submit" onClick={handleSaveList}>
-                                        SAVE LIST
-                                    </Button>
-                                </>
-                                
-                            )}
+                                    </>
+                                ) : (
+                                    <>
+                                        <div>
+                                            <ClearOptions />
+                                        </div>
+                                        <div className="pr-2 pl-2">
+                                            <ReorderList />
+                                        </div>
+                                        <Button className="drop-shadow-sm" color="green" type="submit" onClick={handleSaveList}>
+                                            SAVE LIST
+                                        </Button>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
                 </>
             ) : (
                 <>
                     {list ? (
                         <div className="flex flex-col justify-evenly items-center">
-                        <div className="flex flex-row justify-evenly mb-1">
-                            <div className="p-2 mr-2">
-                                <p><b>Client: </b>{list?.client_name}</p>
-                                <p><b>Date Created: </b>{formatDate(list?.date)}</p>
-                                <p><b>Last Updated: </b></p>
-                                <p>
-                                    <b className="pr-3">URL: </b>
-                                    <Button onClick={handleOpenURL}>
-                                        <i className="external alternate icon !-mr-1"></i>
-                                    </Button>
-                                </p>
+                            <div className="flex flex-row justify-evenly mb-1">
+                                <div className="p-2 mr-2">
+                                    <p><b>Client: </b>{list?.client_name}</p>
+                                    <p><b>Date Created: </b>{formatDate(list?.date)}</p>
+                                    <p><b>Last Updated: </b></p>
+                                    <p>
+                                        <b className="pr-3">URL: </b>
+                                        <Button onClick={handleOpenURL}>
+                                            <i className="external alternate icon !-mr-1"></i>
+                                        </Button>
+                                    </p>
+                                </div>
+                                <div className="flex flex-col justify-evenly items-center">
+                                    <DeleteList list={list} handleCloseModal={handleCloseModal}/>
+                                    <Button className="drop-shadow-sm text-nowrap !bg-[#90B8F8] hover:!bg-[#5F85DB]" type="submit" onClick={handleEditList}>EDIT LIST</Button>
+                                </div>
                             </div>
-                            <div className="flex flex-col justify-evenly items-center">
-                                <DeleteList list={list} handleCloseModal={handleCloseModal}/>
-                                <Button className="drop-shadow-sm text-nowrap !bg-[#90B8F8] hover:!bg-[#5F85DB]" type="submit" onClick={handleEditList}>EDIT LIST</Button>
-
+                            <Divider />
+                            <div className="flex flex-col justify-center items-center pb-5">
+                                <h4 className="text-lg font-semibold mb-4">Options</h4>
                             </div>
-                        </div>
-                        <Divider />
-                        <div className="flex flex-col justify-center items-center pb-5">
-                            <h4 className="text-lg font-semibold mb-4">Options</h4>
-                        </div>
-                        <div className="w-11/12 md:w-full flex justify-center">
-                            <table className="table-auto border rounded-lg">
-                                <thead className="bg-[#d9dadb] border rounded-lg">
-                                    <tr>
-                                        <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Property Name</th>
-                                        <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Rate</th>
-                                        <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Unit</th>
-                                        <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Layout</th>
-                                        <th className="px-2 py-2 text-left text-sm text-nowrap font-bold text-gray-700">Sq Ft</th>
-                                        <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Notes </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {list?.options.map((option, index) => (
-                                        <tr
-                                            key={option.id}
-                                            className={`${index % 2 === 0 ? 'bg-[#232425]' : 'bg-[#26282B]'} text-white hover:bg-gray-100 hover:text-black transition`}
-                                        >
-                                            <td className="px-2 py-2 whitespace-nowrap text-sm">{option?.prop_name}</td>
-                                            <td className="px-2 py-2 text-xs">${option?.price}</td>
-                                            <td className="px-2 py-2 text-xs">{option?.unit}</td>
-                                            <td className="px-2 py-2 text-xs">{option?.layout}</td>
-                                            <td className="px-2 py-2 text-xs">{option?.sq_ft}</td>
-                                            <td className="px-2 py-2 text-xs text-center">
-                                                {option?.notes ? (
-                                                    <Popup
-                                                    content={option.notes}
-                                                    trigger={<i className="ellipsis horizontal icon"></i>}
-                                                    />
-                                                ) : ( <></>)}
-                                                
-                                            </td>
+                            <div className="w-11/12 md:w-full flex justify-center">
+                                <table className="table-auto border rounded-lg">
+                                    <thead className="bg-[#d9dadb] border rounded-lg">
+                                        <tr>
+                                            <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Property Name</th>
+                                            <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Rate</th>
+                                            <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Unit</th>
+                                            <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Layout</th>
+                                            <th className="px-2 py-2 text-left text-sm text-nowrap font-bold text-gray-700">Sq Ft</th>
+                                            <th className="px-2 py-2 text-left text-sm font-bold text-gray-700">Notes </th>
                                         </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                                    </thead>
+                                    <tbody>
+                                        {list?.options.map((option, index) => (
+                                            <tr
+                                                key={option.id}
+                                                className={`${index % 2 === 0 ? 'bg-[#232425]' : 'bg-[#26282B]'} text-white hover:bg-gray-100 hover:text-black transition`}
+                                            >
+                                                <td className="px-2 py-2 whitespace-nowrap text-sm">{option?.prop_name}</td>
+                                                <td className="px-2 py-2 text-xs">${option?.price}</td>
+                                                <td className="px-2 py-2 text-xs">{option?.unit}</td>
+                                                <td className="px-2 py-2 text-xs">{option?.layout}</td>
+                                                <td className="px-2 py-2 text-xs">{option?.sq_ft}</td>
+                                                <td className="px-2 py-2 text-xs text-center">
+                                                    {option?.notes ? (
+                                                        <Popup
+                                                        content={option.notes}
+                                                        trigger={<i className="ellipsis horizontal icon"></i>}
+                                                        />
+                                                    ) : ( <></>)}
+                                                    
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     ) : (
                         <>
@@ -181,10 +174,8 @@ function ListDetail({ listID, list, options, property, client, user, set_list_mo
                             </Dimmer>
                         </>
                     )}
-                 
                 </>
             )}
-
         </>
     )
 }
@@ -200,4 +191,4 @@ const mapStateToProps = state => ({
     options: state.listmaker.options
 });
 
-export default connect(mapStateToProps, { set_list_mode, set_search_client, load_list, new_option, set_list_edit, update_list_options, reset_list_mode })(ListDetail);
+export default connect(mapStateToProps, { set_list_mode, load_list, new_option, update_list, reset_list_mode })(ListDetail);
